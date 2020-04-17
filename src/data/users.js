f67140b60ac77cd526f42ad34a9bd1f17b919b38
Chris Watzon <cawatson1993@gmail.com>
2020-04-17 10:49:58 -0600

    Fix spelling

--- src/data/users.js
+++ src/data/users.js
@@ -1,7 +1,7 @@
 const users = [
     {
         title: 'PixieImageBot',
-        description: 'Pixie is a bot for doing image conversions and manipulaiton in Telegram',
+        description: 'Pixie is a bot for doing image conversions and manipulation in Telegram',
         preview: require('./showcase/pixieimagebot.png'),
         link: 'https://t.me/pixieimagebot',
         source: 'https://github.com/watzon/pixiebot'
